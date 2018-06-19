202c677d653cedfcb97205416e5de79a4979525d
ramo0301 <38494239+ramo0301@users.noreply.github.com>
2018-06-19 19:38:23 +0200

    Add files via upload

---
+++ GraphFrame.java
@@ -0,0 +1,94 @@
+package current;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+
+public class GraphFrame extends JFrame {
+	
+	public static final int FRAME_WIDTH = 1000;
+	public static final int FRAME_HEIGHT = 500;
+	
+	GraphModel theModel = new GraphModel();
+	GraphPanel thePanel = new GraphPanel(theModel);
+	
+	public GraphFrame(){
+		
+		super("Graph Frame");
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout() );
+		setBackground(Color.DARK_GRAY);
+		
+		/* INITIALIZE MENU
+		 */
+		JMenu theMenu = new JMenu("Menu");
+		JMenuItem menuAddVertex = new JMenuItem("Add Vertex");
+		JMenuBar bar = new JMenuBar();
+		
+		theMenu.add(menuAddVertex);
+		bar.add(theMenu);
+		setJMenuBar(bar);
+		
+		
+		/* INITIALIZE ACTIONS AND SET TO MENU ITEMS
+		 */
+		Action addVertex = new AddVertexAction();
+		menuAddVertex.setAction(addVertex);
+		
+		/* RELEVANT PANEL IN THE CENTER, BLANK PANELS AS BORDERS
+		 */
+		add(new EmptyPanel() , BorderLayout.NORTH);
+		add(new EmptyPanel() , BorderLayout.SOUTH);
+		add(new EmptyPanel() , BorderLayout.EAST);
+		add(new EmptyPanel() , BorderLayout.WEST);
+		add(thePanel , BorderLayout.CENTER);
+	}
+
+	
+	/* INNNER CLASS USED TO GENERATE EMPTY PANELS
+	 */
+	public class EmptyPanel extends JPanel {
+		public EmptyPanel(){
+			super();
+			setBackground(Color.DARK_GRAY);
+		}
+	}
+	
+	private class AddVertexAction extends AbstractAction {
+		
+		/* DEFAULT X AND Y VALUES
+		 */
+		int x=300, y=200;
+		
+		
+		/* TWO CONSTRUCTORS
+		 */
+		public AddVertexAction(){
+			super("Add Vertex");
+		}
+		
+		public AddVertexAction(int inputX, int inputY){
+			super("Add Vertex");
+			x = inputX;
+			y = inputY;
+		}
+		
+		
+		public void actionPerformed(ActionEvent e) {
+			theModel.addVertex(x,y);
+			thePanel.repaint();
+			
+		}
+	}
+
+}
